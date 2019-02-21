c244405016a7c02d9fada145f7733af463d6480a
Dalin  Oluoch <mcdalinoluoch@gmail.com>
2019-02-21 18:49:14 +0300

    [Feature] Implement signup validations

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,6 +1,8 @@
 import { UserInputError } from 'apollo-server-express';
+import Joi from 'joi';
 import mongoose from 'mongoose';
 import { User } from '../models';
+import { Signup } from '../schemas';
 
 export default {
   Query: {
@@ -15,7 +17,8 @@ export default {
     }
   },
   Mutation: {
-    signUp: (root, args, context, info) => {
+    signUp: async (root, args, context, info) => {
+      await Joi.validate(args, Signup, { abortEarly: false });
       return User.create(args);
     }
   }
